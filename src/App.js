39d6936aaa2362ffe76a631d02a814029b00c9e1
leiahgonzalez <75752504+leiahgonzalez@users.noreply.github.com>
2021-11-09 22:06:47 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from "../src/components/NavBar";
 import SimpleMenu from './components/SimpleMenu';
+import { CartWidget } from './components/CartWidget';
+import { ItemListContainer } from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <SimpleMenu/>
+      <CartWidget/>
+      <ItemListContainer greeting="Hola Coders!"/>
     </div>
   );
 }
---
+++ src/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ItemListContainer = ( {greeting} ) => {
+    return (
+        <div class="ItemListContainer">
+            <h2> {greeting} </h2>
+            <hr/>
+        </div>
+    )
+}
